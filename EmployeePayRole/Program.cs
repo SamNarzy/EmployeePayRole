4af6f586c93d3956a9767ea6c80cda72b152d77c
SamNarzy <samirnarzary004@gmail.com>
2022-10-20 20:29:06 +0530

    new repo

---
+++ EmployeePayRole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeePayRole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(0,2);//0 & 1 random number
+            Console.WriteLine("Hello World!");
+            
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("absent");
+            }
+
+            switch(value)
+            {
+                case 0: Console.WriteLine("Present");break;
+                default: Console.WriteLine("absent");break;
+            }
+        }
+    }
+}
